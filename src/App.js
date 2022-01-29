06e09738679b4a667d68167115948a00334eebc6
riccardoamici-ricky <>
2022-01-29 13:26:06 +0100

    pause

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { useState, useEffect} from 'react';
-import Dropdown from "./components/Dropdown";
-import Footer from './components/Footer';
-import Navbar from "./components/Navbar";
-import GlobalStyle from "./globalStyles";
-import { Routes, Route} from 'react-router-dom';
-import Home from './pages';
-import About from './pages/About';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import ScrollToTop from './components/ScrollToTop';
-import Contact from './pages/Contact';
-import Restaurant from './pages/Restaurant';
-import Executive from './pages/Executive';
-import Menu from './pages/Menu';
-import Wines from './pages/Wines';
-import PopUp from './components/PopUp';
+// import Dropdown from "./components/Dropdown";
+// import Footer from './components/Footer';
+// import Navbar from "./components/Navbar";
+// import GlobalStyle from "./globalStyles";
+// import { Routes, Route} from 'react-router-dom';
+// import Home from './pages';
+// import About from './pages/About';
+// import Aos from 'aos';
+// import 'aos/dist/aos.css';
+// import ScrollToTop from './components/ScrollToTop';
+// import Contact from './pages/Contact';
+// import Restaurant from './pages/Restaurant';
+// import Executive from './pages/Executive';
+// import Menu from './pages/Menu';
+// import Wines from './pages/Wines';
+// import PopUp from './components/PopUp';
 
 function App() {
 
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen)
-  }
+  // const toggle = () => {
+  //   setIsOpen(!isOpen)
+  // }
 
 
-  useEffect(() => {
-    Aos.init({})
-  }, [])
+  // useEffect(() => {
+  //   Aos.init({})
+  // }, [])
 
   return (
     <>
